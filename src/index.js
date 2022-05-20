1de8576c3082326956623af4be2b25525af61a7a
Nausher Rao <SherRao15@gmail.com>
2022-05-20 03:04:16 -0400

    Update root files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
 import { AppContainer } from "@layouts";
 import { Router } from "./Router";
 import { Theme } from "./Theme";
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Html } from "@react-three/drei";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <AppContainer>
-            <Theme>
-                <Router />
-            </Theme>
-        </AppContainer>
-    </React.StrictMode>,
-    document.getElementById("root")
+    <AppContainer />,
+    document.getElementById("root"),
 );
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,18 +1,38 @@
-import React from 'react';
-import Styled from 'styled-components';
+import React from "react";
+import { PageContainer, HorizontalSection } from "@layouts";
+import { Grid } from "@mantine/core";
+import { Center, Container, Stack, Title, Text } from "@mantine/core";
 
-import { PageContainer, HorizontalSection, VerticalSection } from "@layouts";
-import { Title, Header, Body } from "@atoms";
-
-export default function LandingPage({theme}) {
+export default function LandingPage() {
     return (
-        <PageContainer>
-            <HorizontalSection id="hero1" backgroundColor="red">
-                <Title text="Hey, my name is Nausher"/>
-                <Header text="this is a header" />
-                <Body text="this is a body" />
-            </HorizontalSection>
+        <Container fluid height="100vh" style={{backgroundColor: "red"}}>
+            <Center>
+                <Stack>
+                    <Container>
+                        <Title>Hey, my name is Nausher</Title>
+                    </Container>
 
-        </PageContainer>
+                    <Container>
+                        <Text>
+                    My new website is currently under construction, for my old website<a href="/old"> click here</a>!
+                        </Text>
+                    </Container>
+
+                    <Container>
+                        <Text>
+                    You can find my resume<a href="/resume.pdf"> here</a>!
+                        </Text>
+                    </Container>
+
+                    <Container>
+                        <Grid>
+                            <Grid.Col span={4}>1</Grid.Col>
+                            <Grid.Col span={4}>2</Grid.Col>
+                            <Grid.Col span={4}>3</Grid.Col>
+                        </Grid>
+                    </Container>
+                </Stack>
+            </Center>
+        </Container>
     );
 }
