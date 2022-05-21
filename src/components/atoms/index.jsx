3f813fc38dd408415e85b39b371a95b551a211cf
Nausher Rao <SherRao15@gmail.com>
2022-05-20 23:51:24 -0400

    Delete old atomic-structure components

--- src/components/atoms/index.jsx
+++
@@ -1,6 +0,0 @@
-import Title from './Title';
-import Header from "./Header";
-import Body from "./Body";
-import Picture from "./Picture";
-
-export { Title, Header, Body, Picture };
--- src/components/layouts/index.jsx
+++
@@ -1,6 +0,0 @@
-import AppContainer from "./AppContainer";
-import PageContainer from "./PageContainer";
-import HorizontalSection from "./HorizontalSection";
-import VerticalSection from "./VerticalSection";
-
-export { AppContainer, PageContainer, HorizontalSection, VerticalSection };
