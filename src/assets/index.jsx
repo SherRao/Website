7f4a8113125558a29c0eabde1e75c7f4afb98e06
Nausher Rao <SherRao15@gmail.com>
2023-09-01 07:25:15 -0400

    Delete useless assets

--- src/assets/index.jsx
+++
@@ -1,7 +0,0 @@
-import Background from "./background.json";
-import Loading from "./loading.json";
-import Notification from "./notification.json";
-import SpinningThing from "./spinning_thing.json";
-import Favicon from "./favicon.png";
-
-export {Background, Loading, Notification, SpinningThing, Favicon};
