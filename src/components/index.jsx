74e86c7180dd52f77e261b84297ee387e4723a1c
Nausher Rao <SherRao15@gmail.com>
2024-02-08 02:14:44 -0500

    Start work on new background effect

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,3 @@
-export {App} from "./App";
-export {AppContainer} from "./AppContainer";
-export {SocialMediaBar} from "./SocialMediaBar";
-export {Spacer} from "./Spacer";
+export { App } from "./App";
+export { AppContainer } from "./AppContainer";
+export { Spacer } from "./Spacer";
