42b737b6d664c5bf1326dd055c1419f893e4eebd
Nausher Rao <SherRao15@gmail.com>
2024-06-01 17:57:10 -0400

    Start work on Valorant styled header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactFullpage from "@fullpage/react-fullpage";
 import { Spacer, SocialMediaBar } from "@components";
+import TextTransition, { presets } from "react-text-transition";
 
 const programmingLanguagesExt = [".js", ".ts", ".css", ".html", ".java", ".py", ".c", ".cpp", ".rb"];
 const programmingLanguagesJsx = programmingLanguagesExt.map((lang, i) =>
@@ -13,54 +13,46 @@ const programmingLanguagesJsx = programmingLanguagesExt.map((lang, i) =>
     </span>
 );
 
-const SEL = "custom-section";
-const SECTION_SEL = `.${SEL}`;
+const jobs = ["Software Engineer", "Product Manager", "Entreprenuer", "Leader"];
 
 const App = () => {
+    const [index, setIndex] = React.useState(0);
+    React.useEffect(() => {
+        const intervalId = setInterval(() => {
+            setIndex(index => index + 1);
+        }, 1000);
+        return () => clearTimeout(intervalId);
+    } , []);
+
     return (
-        <ReactFullpage
-            debug
-            sectionSelector={SECTION_SEL}
-            // anchors={["firstPage", "secondPage", "thirdPage"]}
-            licenseKey="gplv3-license"
-            scrollingSpeed={1000}
-            render={() => (
-                <ReactFullpage.Wrapper>
-                    <div className="custom-section">
-                        <AppContainer>
-                            <ContentContainer>
-                                <span>
-                                    <h1>{"Hey, I'm "}<strong>{"Nausher Rao"}</strong>{"! 👋"}</h1>
-                                </span>
-                                <p>
-                                    {"I'm a 5th-year university student and have been programming since I was 9 years old (big nerd... I know). I love to keep myself busy (maybe a bit too busy) with a bunch of extra curriculars and work stuff. Some of the programming languages I've been using daily recently include: "}
-                                </p>
-                                <p>
-                                    {programmingLanguagesJsx}
-                                </p>
+        <AppContainer>
+            <ContentContainer>
+                <span>
+                    <h1>{"Hey, I'm "}<strong>{"Nausher Rao"}</strong>{"! 👋"}</h1>
+                </span>
+                <p>
+                    {"I'm a "}
+                </p>
+                <TextTransition className="test" springConfig={presets.wobbly} style={{textAlign: "center"}}>
+                    <h1>
+                        {jobs[index % jobs.length]}
+                    </h1>
+                </TextTransition>
+                <p>
+                    {" and have been programming since I was 9 years old (big nerd... I know). I love to keep myself busy (maybe a bit too busy) with a bunch of extra curriculars and work stuff. Some of the programming languages I've been using daily recently include: "}
+                </p>
+                <p>
+                    {programmingLanguagesJsx}
+                </p>
 
-                                <Spacer h={50} />
+                <Spacer h={50} />
 
-                                <p>
-                                    {"Feel free to hit me up on any of my socials, or check out my old site or resume below! 🙃"}
-                                </p>
-                                <SocialMediaBar />
-                            </ContentContainer>
-                        </AppContainer>
-                    </div>
-
-                    <div className="custom-section" style={{width: "100vw", height: "100vh", backgroundColor: "red"}}>
-                        <h1>s2</h1>
-                    </div>
-
-                    <div className="custom-section" style={{width: "100vw", height: "100vh", backgroundColor: "blue"}}>
-                        <h1>s3</h1>
-                    </div>
-
-
-                </ReactFullpage.Wrapper>
-            )}
-        />
+                <p>
+                    {"Feel free to hit me up on any of my socials, or check out my old site or resume below! 🙃"}
+                </p>
+                <SocialMediaBar />
+            </ContentContainer>
+        </AppContainer>
     );
 };
 
