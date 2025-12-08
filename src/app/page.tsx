ca8d2edb7914c791fe1c7d3c573d595f9cd29870
Nausher Rao <SherRao15@gmail.com>
2025-12-08 02:04:42 -0500

    Update page with new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import React from 'react';
-import { BackgroundBlob, HeroSection } from "../components"
+import { BackgroundBlob, HeroSection, AboutSection, Navbar } from "../components"
 
 
 const Home = () => {
   return (
-    <div id="app-container" className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-background z-0">
+    <div id="app-container" className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-background z-0">
       <BackgroundBlob />
+      {/* <Navbar /> */}
       <HeroSection />
+      {/* <AboutSection /> */}
 
     </div>
   );
