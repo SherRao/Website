c06e2e2948c2657ea61e4f3bca658de2444393a7
Nausher Rao <SherRao15@gmail.com>
2025-12-08 02:03:54 -0500

    Start work on Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Image from "next/image";
+
+interface CardProps {
+    images?: typeof Image[];
+    title?: string;
+    description?: string;
+};
+
+const Card = ({images, title, description}: CardProps) => {
+    
+
+    return (
+        <>
+        </>
+    );
+};
+
+export default Card;
