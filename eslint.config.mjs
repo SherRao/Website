919015dbcac445e8065499df007f7bc3f8497f3b
Nausher Rao <SherRao15@gmail.com>
2025-12-09 03:51:24 -0500

    Update eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,13 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+
+  {
+    rules: {
+      semi: "error",
+      quotes: ["error", "double"]
+    },
+  }
 ]);
 
 export default eslintConfig;
