a23cb5f11b2d53f50b3c99f35f4e9b41c9232a38
Nausher Rao <SherRao15@gmail.com>
2025-12-09 04:33:20 -0500

    hide projects page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import React from 'react';
+import React from "react";
 import { PageContainer } from "@/containers";
-import { AboutSection, HeroSection, ProjectSection } from '@/sections';
-import { BackgroundBlob, BounceCards, SocialMediaIcons, Navbar } from "@/components"
+import { AboutSection, HeroSection, ProjectSection } from "@/sections";
+import { BackgroundBlob, Navbar } from "@/components";
 
 /**
  * 
@@ -15,14 +15,12 @@ const Home = () => {
       <PageContainer>
         <BackgroundBlob />
         <Navbar />
-        {/* <div id="sections" className="min-h-screen min-w-screen flex flex-col justify-center items-center"> */}
         <HeroSection />
         <AboutSection />
-        <ProjectSection />
-        {/* </div> */}
+        {/* <ProjectSection /> */}
       </PageContainer>
     </>
   );
-}
+};
 
 export default Home;
