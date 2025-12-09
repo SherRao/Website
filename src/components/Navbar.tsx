ad63e157b6b023a04cf27f89247450fcbda74cb3
Nausher Rao <SherRao15@gmail.com>
2025-12-09 04:01:26 -0500

    Update logo in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,23 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
-import { SocialMediaIcons, NavbarCTA } from "@/components";
+import { SocialMediaIcons } from "@/components";
 
 const Navbar = () => {
     return (
         <nav id="navbar" className="fixed w-full h-auto flex flex-row items-center justify-between top-0 py-10 px-15">
-            <div className="">
-                <Image src="./next.svg" width={128} height={128} alt="Logo" />
-            </div>
+            <h1 className="">
+                <span
+                    className="pointer-events-none text-3xl font-bold tracking-tight drop-shadow-md bg-linear-to-r from-violet-400 via-fuchsia-400 to-emerald-400 bg-clip-text text-transparent"
+                    style={{ letterSpacing: "0.02em" }}
+                >
+                    nausher <span className="font-bold">rao</span>
+                </span>
+            </h1>
 
             <SocialMediaIcons />
         </nav>
-    )
-}
+    );
+};
 
 export default Navbar;
