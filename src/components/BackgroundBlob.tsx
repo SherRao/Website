6d19aab89f01e7c639f5af5fa3600d5d24c8b5d2
Nausher Rao <SherRao15@gmail.com>
2025-12-09 03:21:03 -0500

    Cleanup and finish background blob component

--- src/components/BackgroundBlob.tsx
+++ src/components/BackgroundBlob.tsx
@@ -2,6 +2,10 @@
 
 import React, { useState, useEffect } from "react";
 
+/**
+ * A backgroud circle with a gradient. Also has a screen sized transparent div with a backdrop-blur filter. 
+ * 
+ */
 const BackgroundBlob = () => {
   const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
   useEffect(() => {
@@ -12,18 +16,17 @@ const BackgroundBlob = () => {
   }, []);
 
   return (
-    <>
-      <div id="blob" className="fixed bg-white aspect-square h-[500px] left-[50%] top-[50%] rounded-full z-1"
+    <div id="blob-container" className="fixed left-0 top-0 min-w-screen min-h-screen -z-1000">
+      <div id="blob" className="fixed left-0 top-0 aspect-square h-[500px] rounded-full -z-1000"
         style={{
           background: "linear-gradient(to right, aquamarine, mediumpurple)",
           left: cursorPos.x - 250,
           top: cursorPos.y - 250,
           animation: "rotateBlob 20s infinite, top 3s linear 0s 1 normal forwards, left 3s linear 0s 1 normal forwards"
         }} />
-      <div id="blur" className="fixed w-full h-full backdrop-blur-[200px] bg-none z-2" />
-    </>
+      <div id="blur" className="fixed left-0 top-0 min-w-screen min-h-screen backdrop-blur-[500px] -z-999" />
+    </div>
   );
-
 };
 
 export default BackgroundBlob;
