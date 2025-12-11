d7a51c002f9db8b0e4db3963e0dd2acbaf20de09
Nausher Rao <SherRao15@gmail.com>
2025-12-10 21:11:47 -0500

    Remove old component exports from index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,7 @@
 import BackgroundBlob from "./BackgroundBlob";
-import BounceCards from "./BounceCards";
-import Card from "./Card";
 import DownButton from "./DownButton";
 import Navbar from "./Navbar";
 import NavbarCTA from "./NavbarCTA";
 import SocialMediaIcons from "./SocialMediaIcons";
 
-export { BackgroundBlob, BounceCards, Card, DownButton, Navbar, NavbarCTA, SocialMediaIcons };
+export { BackgroundBlob, DownButton, Navbar, NavbarCTA, SocialMediaIcons };
