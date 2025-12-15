10e6a68322704bc225ef03d465a75e72a665427a
Nausher Rao <SherRao15@gmail.com>
2025-12-14 19:02:58 -0500

    Add curved loop to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { PageContainer } from "@/containers";
 import { AboutSection, HeroSection, ProjectSection } from "@/sections";
 import { BackgroundBlob, Navbar } from "@/components";
+import CurvedLoop from "@/components/CurvedText";
 
 /**
  * 
@@ -13,11 +14,25 @@ const Home = () => {
   return (
     <>
       <PageContainer>
-        <BackgroundBlob />
+        <BackgroundBlob /> {/** z-index of -1000 and -999 */}
         <Navbar />
         <HeroSection />
+        {/* <div className="flex items-center justify-center h-min w-screen p-4 sm:p-8"> */}
+        {/* <div className="relative w-full flex items-center justify-center px-4 sm:px-12"> */}
+        <CurvedLoop
+          marqueeText="PROGRAMMING ✦ COMMUNITY ✦ LEADERSHIP ✦ BUILDING ✦"
+          speed={1}
+          tailwindSize="text-5xl"
+          curveAmount={300}
+          direction="left"
+          interactive={false}
+          className="text-foreground"
+        />
+        {/* </div> */}
+        {/* </div> */}
+
         <AboutSection />
-        {/* <ProjectSection /> */}
+        <ProjectSection />
       </PageContainer>
     </>
   );
