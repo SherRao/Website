45230c98daf3544b4afc8837b5f54615a4012f16
Nausher Rao <SherRao15@gmail.com>
2025-12-18 01:21:39 -0500

    Implement basic mobile optimisation for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,15 +7,16 @@ import { SocialMediaIcons } from "@/components";
 const Navbar = () => {
     return (
         <motion.nav id="navbar"
-            className="fixed w-full h-auto flex flex-row items-center justify-between top-0 py-10 px-15"
+            className="fixed w-full h-auto flex flex-row items-center justify-between top-0 py-10 px-5 lg:px-15"
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.7, ease: "easeOut" }}
         >
             <h1 className="">
                 <span
-                    className="pointer-events-none text-3xl font-bold tracking-tight drop-shadow-md bg-linear-to-r from-violet-400 via-fuchsia-400 to-emerald-400 bg-clip-text text-transparent"
-                    style={{ letterSpacing: "0.02em" }}
+                    className="text-lg lg:text-3xl font-bold text-nowrap tracking-wide  
+                    bg-linear-to-r from-violet-400 via-fuchsia-400 to-emerald-400 bg-clip-text text-transparent drop-shadow-md
+                    pointer-events-none"
                 >
                     nausher <span className="font-bold">rao</span>
                 </span>
