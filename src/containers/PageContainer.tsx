6c4c777b684531a09bf085f71313112d0ed67ab8
Nausher Rao <SherRao15@gmail.com>
2025-12-18 01:25:41 -0500

    Update PageContainer to be a client side component

--- src/containers/PageContainer.tsx
+++ src/containers/PageContainer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 
 export type PageContainerProps = {
@@ -5,7 +7,7 @@ export type PageContainerProps = {
 }
 
 const PageContainer = ({ children }: PageContainerProps) => (
-    <div id="app-container" className="w-full min-h-screen flex flex-col items-center relative z-3">
+    <div id="app-container" className="w-full min-h-screen flex flex-col items-center relative z-3 gap-50">
         {children}
     </div>
 );
