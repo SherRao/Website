b1b85bd0fb53576e77da2ff38845524cdad3c4f5
Nausher Rao <SherRao15@gmail.com>
2025-12-18 01:24:46 -0500

    Add picsum.picture to image remote patterns in nextjs config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [new URL("https://picsum.photos/**")],
+  },
 };
 
 export default nextConfig;
