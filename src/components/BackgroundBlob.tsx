617dd5e230398bf632cfc805924c241f104bd947
Nausher Rao <SherRao15@gmail.com>
2025-12-18 01:25:06 -0500

    Implement basic mobile optimisation for background blob

--- src/components/BackgroundBlob.tsx
+++ src/components/BackgroundBlob.tsx
@@ -16,7 +16,7 @@ const BackgroundBlob = () => {
   }, []);
 
   return (
-    <div id="blob-container" className="fixed left-0 top-0 min-w-screen min-h-screen -z-1000">
+    <div id="blob-container" className="hidden lg:block fixed left-0 top-0 min-w-screen min-h-screen -z-1000">
       <div id="blob" className="fixed left-0 top-0 aspect-square h-[1000px] rounded-full -z-1000"
         style={{
           background: "linear-gradient(to right, aquamarine, mediumpurple)",
