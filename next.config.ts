266e5ff5bece1c51bcd247adfe0d310e9e9e3825
Nausher Rao <SherRao15@gmail.com>
2025-12-19 01:22:58 -0500

    Update nextjs config with /resume redirect

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  images: {
-    remotePatterns: [new URL("https://picsum.photos/**")],
-  },
+    reactCompiler: true,
+
+    images: {
+        remotePatterns: [new URL("https://picsum.photos/**")],
+    },
+
+    async redirects() {
+        return [
+            {
+                source: "/resume",
+                destination: "/Resume%20-%20Nausher%20Rao.pdf",
+                permanent: true,
+            }
+        ];
+    },
 };
 
 export default nextConfig;
