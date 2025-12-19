ce2a2f366c474824ded37412c446258d3d028f2d
Nausher Rao <SherRao15@gmail.com>
2025-12-19 01:49:34 -0500

    Remove old files from weird branch history merge

--- src/components/index.jsx
+++
@@ -1,4 +0,0 @@
-export {App} from "./App";
-export {AppContainer} from "./AppContainer";
-export {SocialMediaBar} from "./SocialMediaBar";
-export {Spacer} from "./Spacer";
--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { AppContainer } from "@components";
-import "./styles.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <AppContainer />
-);
