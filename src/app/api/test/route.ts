e4558ddc1946eb6207c38ad9e459dfdeab090c30
Nausher Rao <SherRao15@gmail.com>
2025-12-19 01:50:56 -0500

    Remove test api endpoint

--- src/app/api/test/route.ts
+++
@@ -1,8 +0,0 @@
-import type { NextRequest } from 'next/server'
-
-export async function GET(request: NextRequest) {
-    const url = request.nextUrl;
-
-    return Response.json({ message: `test api response yop` })
-
-}
