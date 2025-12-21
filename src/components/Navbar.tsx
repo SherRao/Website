4a937f81d2585690d4f68d2aa6344cb49690c624
Nausher Rao <SherRao15@gmail.com>
2025-12-21 02:17:25 -0500

    Add scroll hide animation to navbart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { motion } from "motion/react";
+import { motion, useMotionValueEvent, useScroll } from "motion/react";
 import { SocialMediaIcons } from "@/components";
 
 const NavbarCTA = () => {
@@ -26,24 +26,35 @@ const NavbarCTA = () => {
 };
 
 const Navbar = () => {
+    const { scrollYProgress } = useScroll();
+    const [visible, setVisible] = React.useState(true);
+    useMotionValueEvent(scrollYProgress, "change", (current) => {
+        console.log(current);
+        if (scrollYProgress.get() > 0.10)
+            setVisible(false);
+
+        else setVisible(true);
+
+    });
+
     return (
-        <motion.nav id="navbar"
-            className="fixed w-full h-auto flex flex-row items-center justify-between top-0 py-10 px-5 lg:px-15"
+        <motion.nav
+            id="navbar"
+            className="fixed w-full h-auto flex flex-row items-center justify-between top-0 py-10 px-5 lg:px-15 z-1000"
             initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.7, ease: "easeOut" }}
+            animate={{ opacity: visible ? 1 : 0, y: visible ? 0 : -100 }}
+            transition={{ duration: 0.4, ease: "easeOut" }}
         >
-            <h1 className="">
-                <span
-                    className="text-lg lg:text-3xl font-bold text-nowrap tracking-wide  
+            <span
+                className="text-lg lg:text-3xl font-bold text-nowrap tracking-wide  
                     bg-linear-to-r from-violet-400 via-fuchsia-400 to-emerald-400 bg-clip-text text-transparent drop-shadow-md
                     pointer-events-none"
-                >
-                    nausher <span className="font-bold">rao</span>
-                </span>
-            </h1>
+            >
+                nausher rao
+            </span>
 
             <SocialMediaIcons />
+            <NavbarCTA />
         </motion.nav>
     );
 };
