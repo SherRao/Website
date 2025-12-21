035356a5a15e5fd31ce75fad2ed2bf4df8c35e92
Nausher Rao <SherRao15@gmail.com>
2025-12-21 03:52:23 -0500

    Update ImageCard cause of stupid dyanmic tailwind classes not working

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -4,14 +4,14 @@ import React from "react";
 type ImageCardProps = {
     src: string;
     alt?: string;
-    width?: string;
-    height?: string;
+    width?: number | string;
+    height?: number | string;
     additionalClassNames?: string;
 }
 
 const ImageCard = ({ src, alt, width = "auto", height = "auto", additionalClassNames }: ImageCardProps) => {
     return (
-        <figure className={`relative w-${width} h-${height}  flex flex-col items-center justify-center ${additionalClassNames}`}>
+        <figure className={`relative w-${width} h-${height} flex flex-col items-center justify-center ${additionalClassNames}`} style={{ width, height }}>
             <img src={src} alt={alt} aria-label={alt} className="w-full h-full rounded-2xl" />
         </figure>
     );
