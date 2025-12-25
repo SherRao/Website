db8562b606f9f5b0ca2762d079fa0916a125dbd7
Nausher Rao <SherRao15@gmail.com>
2025-12-25 04:51:06 -0500

    Simplify project gallery component

--- src/components/MasonryGallery.tsx
+++ src/components/MasonryGallery.tsx
@@ -4,25 +4,28 @@ import React from "react";
 import { MasonryGrid, type MasonryGridElement } from "@/components";
 
 export const MasonryGallery = () => {
-    const COLUMN_COUNT = 7;
-    const ROW_COUNT = 4;
+    const COLUMN_COUNT = 4;
+    const ROW_COUNT = 2;
     const count = COLUMN_COUNT * ROW_COUNT;
     const aspectRatios = [
-        { w: 4, h: 5 }, { w: 4, h: 1 }, { w: 4, h: 7 }, { w: 4, h: 3 }, { w: 4, h: 6 },
+        { w: 16, h: 9 }, { w: 12, h: 9 },
+        // { w: 4, h: 7 }, { w: 4, h: 3 }, { w: 4, h: 6 },
     ];
 
     const images: MasonryGridElement[] = Array.from({ length: count }).map((_, i) => {
         const ratio = aspectRatios[i % aspectRatios.length];
-        const width = ratio.w * 100;
-        const height = ratio.h * 100;
+        const width = ratio.w * 33;
+        const height = ratio.h * 33;
         return {
             imageUrl: `https://picsum.photos/id/${(i + 13) * 9 % 1000}/${width}/${height}`,
             width, height, alt: `Gallery image ${i + 1}`,
+            columnTitle: "test 123",
+            columnDescription: "dasdsa"
         };
     });
 
     return (
-        <div className="overflow-hidden">
+        <div className="rotate-5 overflow-x-visible">
             <MasonryGrid images={images} cols={COLUMN_COUNT} />
         </div>
     );
