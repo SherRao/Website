e77fb30bf0b6469c9c09ce9591e0047b2a928150
Nausher Rao <SherRao15@gmail.com>
2025-12-25 03:03:17 -0500

    Refactor h1 stylized header into its own component file

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,8 +5,10 @@ export { ImageCard } from "./ImageCard";
 export { LoadingScreen } from "./LoadingScreen";
 export { MasonryGallery } from "./MasonryGallery";
 export { MasonryGrid } from "./MasonryGrid";
-export type { MasonryGridElement, MasonryGridProps } from "./MasonryGrid";
 export { Navbar } from "./Navbar";
 export { ScrollingText } from "./ScrollingText";
 export { SocialMediaIcons } from "./SocialMediaIcons";
+export { StylizedHeader } from "./StylizedHeader";
 export { TiltedCard } from "./TiltedCard";
+
+export type { MasonryGridElement, MasonryGridProps } from "./MasonryGrid";
