b6071c6aebc50a6b1af355e913076341e989e174
Nausher Rao <SherRao15@gmail.com>
2025-12-25 04:51:38 -0500

    Add ref support to PageContainer

--- src/containers/PageContainer.tsx
+++ src/containers/PageContainer.tsx
@@ -4,10 +4,11 @@ import React from "react";
 
 type PageContainerProps = {
     children: React.ReactNode;
-}
+    ref?: React.Ref<HTMLDivElement>;
+};
 
-export const PageContainer = ({ children }: PageContainerProps) => (
-    <div id="app-container" className="w-full min-h-screen flex flex-col items-center relative z-3 gap-50 overflow-hidden">
+export const PageContainer = ({ children, ref }: PageContainerProps) => (
+    <div id="page-container" ref={ref} className="w-full min-h-screen flex flex-col items-center relative z-3 gap-50 overflow-hidden">
         {children}
     </div>
 );
