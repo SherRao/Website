4033f4bc49aefed2db3b6b40cf1f0c167f8c6b93
Nausher Rao <SherRao15@gmail.com>
2025-12-26 05:32:43 -0500

    Update page.tsx with new sections and spring scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,11 @@
 
 import React from "react";
 import { PageContainer } from "@/containers";
-import { AboutSection, ContactSection, HeroSection, SimpleAboutSection, ProjectsSection } from "@/sections";
+import { AboutSection, ContactSection, HeroSection, SimpleAboutSection, ProjectsSection, Footer } from "@/sections";
 import { BackgroundBlob, DevBanner, LoadingScreen, Navbar, ScrollingText } from "@/components";
-import { motion, useScroll, useTransform, useViewportScroll } from "motion/react";
+import { motion, useSpring, useScroll } from "motion/react";
 
 const loadingTime = 2600;
-
-const ParallaxSection: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    return (
-        <motion.div
-            initial={{ x: -200, opacity: 0 }}
-            whileInView={{ x: 0, opacity: 1 }}
-            transition={{ type: "spring", bounce: 0.25, duration: 0.8 }}
-            viewport={{ once: false, amount: 0.3 }}
-        >
-            {children}
-        </motion.div>
-    );
-};
 
 const Home = () => {
     const [loading, setLoading] = React.useState(true);
@@ -28,49 +15,37 @@ const Home = () => {
         return () => clearTimeout(timer);
     }, []);
 
+    const { scrollY } = useScroll();
+    const ySpring = useSpring(scrollY, {
+        stiffness: 100,
+        damping: 10,
+        mass: 1,
+    });
 
     if (loading)
         return <LoadingScreen fadeOutTime={loadingTime} />;
 
     return (
         <PageContainer>
-            <BackgroundBlob /> {/* z-index of -1000 and -999 */}
-
-            {/* Navbar parallax - static */}
+            {/* z-index of -1000 and -999 */}
+            <BackgroundBlob />
+            {/* z-index of 1000 */}
             <Navbar />
-
-            {/* HeroSection Parallax */}
-            <ParallaxSection>
-                <HeroSection />
-            </ParallaxSection>
-
-            {/* Example: ScrollingText Parallax */}
-            <ParallaxSection>
-                <ScrollingText texts={[" BIZ DEV ⭐ ENTREPRENEUR ⭐", "⭐ PROJECT MANAGER ⭐ SOFTWARE ENGINEER "]} velocity={10} />
-            </ParallaxSection>
-
-
-
-
-            {/* AboutSection or SimpleAboutSection Parallax */}
-            <ParallaxSection>
-                {/* <SimpleAboutSection /> */}
-                <AboutSection />
-            </ParallaxSection>
-
-            {/* ProjectsSection Parallax */}
-            <ParallaxSection>
-                <ProjectsSection />
-            </ParallaxSection>
-
-
-            ContactSection Parallax
-            <ParallaxSection>
-                <ContactSection />
-            </ParallaxSection>
-
-
+            {/* z-index of 1000 */}
             <DevBanner />
+            <motion.div
+                id="content-container"
+                className="w-full flex flex-col items-center gap-25 z-3 will-change-transform overflow-y-visible"
+            // style={{ y: ySpring }}
+            >
+                <HeroSection />
+                <AboutSection />
+                <ProjectsSection />
+                <ContactSection />
+                {/* <ScrollingText texts={[" BIZ DEV ⭐ ENTREPRENEUR ⭐", "⭐ PROJECT MANAGER ⭐ SOFTWARE ENGINEER "]} velocity={10} /> */}
+                {/* <SimpleAboutSection /> */}
+                <Footer />
+            </motion.div>
         </PageContainer >
     );
 };
