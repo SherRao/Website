8452054499accdc9d2300b6bc15cd0fed593e7fd
Nausher Rao <SherRao15@gmail.com>
2026-01-05 05:15:10 -0500

    Update content in AboutSection and ProjectsSection

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -12,41 +12,17 @@ export const AboutSection = () => {
                     {content.about.title}
                 </h1>
 
-                <p className="lg:text-6xl text-lg font-extralight text-gray-100 leading-relaxed tracking-wide">
-                    {"a life-long programmer who also loves "}
-
-                    <span className="text-[#a374ff]">
-                        leadership,
-                    </span>
-
-                    {" "}
-
-                    <span className="text-[#ffd074]">
-                        project management,
-                    </span>
-
-                    {" "}
-
-                    <span className="text-[#17f1d1]">
-                        event planning,
-                    </span>
-
-                    {" "}
-
-                    <span className="text-[#ffd074]">
-                        and live production.
-                    </span>
-                </p>
+                {content.about.description()}
             </div>
 
             <div id="about-content" className="gap-50 flex flex-row justify-between items-center">
                 <img src="/model.png" className="w-2xl" alt="" />
 
                 <p className="w-300 wrap-normal lg:text-4xl text-lg text-left font-light text-gray-100 leading-relaxed">
-                    {"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl dolor, tincidunt sit amet metus vitae, ultricies pellentesque enim. Etiam rutrum eleifend viverra. Integer aliquam congue urna. Cras nec mauris ac turpis euismod tempus vitae a lectus."}
+                    {content.about.paragraph1}
                     <br />
                     <br />
-                    {"Aenean at magna nisi. In suscipit, enim sagittis suscipit vulputate, quam dui blandit turpis, quis congue urna erat non arcu. Etiam consectetur diam pulvinar erat blandit, eu euismod ipsum faucibus. Donec id vestibulum turpis, ac scelerisque ipsum. Etiam ipsum metus, pharetra non urna nec, mollis luctus odio. Sed ligula metus, pretium vel nisl ut, convallis elementum orci. Duis euismod laoreet tempus."}
+                    {content.about.paragraph2}
                 </p>
             </div>
         </SectionContainer>
--- src/sections/ProjectsSection.tsx
+++ src/sections/ProjectsSection.tsx
@@ -3,29 +3,17 @@
 import React from "react";
 import { SectionContainer } from "@/containers";
 import { MasonryGallery } from "@/components";
+import { content } from "@/resources";
 
 export const ProjectsSection = () => {
     return (
         <SectionContainer id="about" addionalClassName="min-h-screen py-100 gap-100 flex flex-col items-center justify-center">
             <div className="flex flex-col px-75 text-right items-end">
                 <h2 className="text-5xl font-black text-right text-[#a374ff] text-nowrap self-start uppercase drop-shadow-sm">
-                    Work
+                    {content.projects.title}
                 </h2>
 
-                <p className="text-6xl text-left font-extralight text-gray-100 leading-relaxed tracking-wide">
-                    <span className="text-[#a374ff]">
-                        Lorem ipsum
-                    </span>
-                    {" dolor sit amet, "}
-                    <span className="text-[#17f1d1]">
-                        consectetur adipiscing elit.
-                    </span>
-                    {" Morbi maximus "}
-                    <span className="underline underline-offset-4 decoration-[#a374ff]/80">
-                        ultrices mi
-                    </span>
-                    {", ultricies vestibulum."}
-                </p>
+                {content.projects.description()}
             </div>
 
             <MasonryGallery />
