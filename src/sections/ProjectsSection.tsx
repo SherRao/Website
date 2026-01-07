42a88a717a3f52363c33a89e618dfc1a2769c9ff
Nausher Rao <SherRao15@gmail.com>
2026-01-07 02:58:28 -0500

    Fix ProjectsSection ID being set to 'about'

--- src/sections/ProjectsSection.tsx
+++ src/sections/ProjectsSection.tsx
@@ -7,7 +7,7 @@ import { content } from "@/resources";
 
 export const ProjectsSection = () => {
     return (
-        <SectionContainer id="about" addionalClassName="min-h-screen py-100 gap-100 flex flex-col items-center justify-center">
+        <SectionContainer id="projects" addionalClassName="min-h-screen py-100 gap-100 flex flex-col items-center justify-center">
             <div className="flex flex-col px-75 text-right items-end">
                 <h2 className="text-5xl font-black text-right text-[#a374ff] text-nowrap self-start uppercase drop-shadow-sm">
                     {content.projects.title}
