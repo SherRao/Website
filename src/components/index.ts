53b6105794a56a5200f8bef42cdca53054d48e1b
Nausher Rao <SherRao15@gmail.com>
2026-01-07 06:01:32 -0500

    Organize some components into subfolders

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,15 +1,19 @@
 export { BackgroundBlob } from "./BackgroundBlob";
 export { DevBanner } from "./DevBaner";
-export { DownButton } from "./DownButton";
 export { ImageCard } from "./ImageCard";
 export { LoadingScreen } from "./LoadingScreen";
 export { MasonryGallery } from "./MasonryGallery";
 export { MasonryGrid } from "./MasonryGrid";
 export { Navbar } from "./Navbar";
 export { RotatingModel } from "./RotatingModel";
-export { ScrollingText } from "./ScrollingText";
 export { SocialMediaIcons } from "./SocialMediaIcons";
-export { StylizedHeader } from "./StylizedHeader";
 export { TiltedCard } from "./TiltedCard";
+
+export { DownButton } from "./buttons/DownButton";
+
+export { Body } from "./text/Body";
+export { Header } from "./text/Header";
+export { ScrollingText } from "./text/ScrollingText";
+export { StylizedHeader } from "./text/StylizedHeader";
 
 export type { MasonryGridElement, MasonryGridProps } from "./MasonryGrid";
