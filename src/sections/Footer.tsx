4979a46e794c8ae454992c55e5ff8ae26b377fb8
Nausher Rao <SherRao15@gmail.com>
2026-01-13 18:35:21 -0500

    Fix up footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import React from "react";
+import { footer } from "@/content";
 import { SectionContainer } from "@/containers";
 import { Body, SocialMediaIcons, StylizedHeader } from "@/components";
-import { about, footer } from "@/content";
 
 export const Footer = () => {
     return (
-        <SectionContainer id="footer">
-            <div id="footer-content" className="w-screen min-h-full flex flex-col items-center justify-center gap-25 p-20 text-center  z-4 bg-transparent backdrop-brightness-25 backdrop-blur-3xl">
+        <SectionContainer.HorizontalFlex id="footer">
+            <div id="footer-content" className="w-screen p-20 z-4 bg-transparent backdrop-brightness-25 backdrop-blur-3xl">
                 <div className="w-full h-fit flex justify-end items-center">
                     <Body className="w-150" bodyClassName="text-right"
                         body={footer.sourceCode} />
@@ -22,6 +22,6 @@ export const Footer = () => {
             </div>
 
             <div id="footer-bg" className="w-screen h-full absolute bottom-0 left-0 z-3 bg-[url(/japan.jpg)] bg-no-repeat bg-cover bg-left" />
-        </SectionContainer>
+        </SectionContainer.HorizontalFlex>
     );
 };
